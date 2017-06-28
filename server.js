e433d85b91da7f8e7fa2853b1ba8a1f29d7e5eff
jonspieg <jonspieg@users.noreply.github.com>
2017-06-27 21:57:59 -0700

    getting out dir from env variable

--- server.js
+++ server.js
@@ -13,15 +13,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const outdir = process.env.OUT_DIR || 'public';
+
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, outdir)));
 
 // Set our api routes
 app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, outdir,'/index.html'));
 });
 
 /**
