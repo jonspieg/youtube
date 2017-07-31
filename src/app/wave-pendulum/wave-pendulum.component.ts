597312e08f626bf9ecc518fb926ab31f11d64321
jonspieg <jonspieg@users.noreply.github.com>
2017-07-31 10:53:48 -0700

    thikening stroke size

--- src/app/wave-pendulum/wave-pendulum.component.ts
+++ src/app/wave-pendulum/wave-pendulum.component.ts
@@ -16,22 +16,11 @@ export class WavePendulumComponent implements OnInit {
 
   ngOnInit() {
       this.canvas = <HTMLCanvasElement>document.getElementById('wave_canvas');
-      this.canvas.style.width  = '800px';
-      this.canvas.style.height = '450px';
+      this.canvas.style.width  = ''+0.5*window.innerWidth+'px';
+      this.canvas.style.height = ''+0.5*window.innerWidth*9/16+'px';
       this.canvas.width=1920
       this.canvas.height=1080
       this.context= this.canvas.getContext('2d');
-      // var centerX = canvas.width / 2;
-      // var centerY = canvas.height / 2;
-      // var radius = 70;
-
-      // context.beginPath();
-      // context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-      // context.fillStyle = 'green';
-      // context.fill();
-      // context.lineWidth = 5;
-      // context.strokeStyle = '#003300';
-      // context.stroke();
 
       setInterval(()=>{this.drawCanvas()}, 1000/this.frameRate);
   }
@@ -42,13 +31,17 @@ export class WavePendulumComponent implements OnInit {
     this.frame++;
 
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    let P=54
-    for(let i=0;i<12;i++)
+    let P=54;
+    let N=12;
+    
+    for(let i=0;i<N;i++)
     {
       this.context.beginPath()
       this.context.arc(860+400*Math.cos(5*t*(P+i)/P),970-70*i,90,0,6.3)
       this.context.fill()
-      this.context.strokeStyle='white';this.context.stroke()
+      this.context.strokeStyle='white';
+      this.context.lineWidth=5;
+      this.context.stroke()
     }
   }
 
